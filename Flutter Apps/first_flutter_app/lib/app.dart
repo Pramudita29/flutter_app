c34750490dcccc9a119506d20d0e2b740f0e13a0
Pramudita <pbhattarai0129@gmail.com>
2024-12-09 09:11:40 +0545

    commented unwanted imports

--- Flutter Apps/first_flutter_app/lib/app.dart
+++ Flutter Apps/first_flutter_app/lib/app.dart
@@ -1,7 +1,7 @@
 //import 'package:first_flutter_app/view/column_view.dart';
 //import 'package:first_flutter_app/view/container_view.dart';
-import 'package:first_flutter_app/view/flexible_expanded_view.dart';
-import 'package:first_flutter_app/view/load_image_view.dart';
+//import 'package:first_flutter_app/view/flexible_expanded_view.dart';
+//import 'package:first_flutter_app/view/load_image_view.dart';
 import 'package:first_flutter_app/view/show_snack_bar_view.dart';
 import 'package:flutter/material.dart';
 //import 'view/dashboard_view.dart'; // Import the Dashboard view
