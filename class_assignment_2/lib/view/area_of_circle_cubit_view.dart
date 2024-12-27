db6fd3fd939aff61dbf150697ac601bac003c289
Pramudita <pbhattarai0129@gmail.com>
2024-12-27 13:25:57 +0545

    area of circle created

---
+++ class_assignment_2/lib/view/area_of_circle_cubit_view.dart
@@ -0,0 +1,52 @@
+import 'package:class_assignment_2/cubit/area_of_circle_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class AreaOfCircleView extends StatelessWidget {
+  const AreaOfCircleView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final radiusController = TextEditingController();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Area of Circle'),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: radiusController,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                labelText: 'Radius',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                final radius = double.tryParse(radiusController.text) ?? 0;
+
+                context.read<AreaOfCircleCubit>().calculateArea(radius);
+              },
+              child: const Text('Calculate'),
+            ),
+            const SizedBox(height: 16),
+            BlocBuilder<AreaOfCircleCubit, double>(
+              builder: (context, area) {
+                return Text(
+                  'Area of Circle: $area',
+                  style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
